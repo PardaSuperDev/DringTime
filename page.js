ddd2eb24827120f4242362af58adeccf5c0f208c
yannis300307 <yannis300307@gmail.com>
2024-02-08 09:17:04 +0100

    Ajout du retour en arrière de l'annimation

--- page.js
+++ page.js
@@ -1,9 +1,15 @@
+window.toggled = False;
 function toggle_view(type) {
     if (type === "next_alarm") {
-        let other_elem = document.getElementById("remaining_time");
-        other_elem.style = "max-height: 0px; opacity: 0%";
+        var other_elem = document.getElementById("remaining_time");
     } else if (type === "remaining_time") {
-        let other_elem = document.getElementById("next_alarm");
+        var other_elem = document.getElementById("next_alarm");
+    } else return;
+    
+    if (window.toggled){
+        other_elem.style = "";
+    } else {
         other_elem.style = "max-height: 0px; opacity: 0%";
     }
+    window.toggled = !window.toggled;
 }
