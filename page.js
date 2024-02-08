e194a9442138c659158809fc7deb10e7df3ea938
yannis300307 <yannis300307@gmail.com>
2024-02-08 09:06:31 +0100

    Ajout des transitions

--- page.js
+++ page.js
@@ -1,13 +1,9 @@
 function toggle_view(type) {
     if (type === "next_alarm") {
         let other_elem = document.getElementById("remaining_time");
-        other_elem.style = "opacity: 0%";
-        let current_elem = document.getElementById("next_alarm")
-        current_elem.style = "top: 150px"
+        other_elem.style = "max-height: 0px; opacity: 0%";
     } else if (type === "remaining_time") {
         let other_elem = document.getElementById("next_alarm");
-        other_elem.style = "opacity: 0%";
-        let current_elem = document.getElementById("remaining_time")
-        current_elem.style = "bottom: 150px"
+        other_elem.style = "max-height: 0px; opacity: 0%";
     }
 }
