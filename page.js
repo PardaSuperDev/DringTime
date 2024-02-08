45507cb97a6489fa561b964c5533b066d10c27d9
yannis300307 <yannis300307@gmail.com>
2024-02-08 09:57:11 +0100

    Ajout boutton settings

--- page.js
+++ page.js
@@ -5,11 +5,15 @@ function toggle_view(type) {
     } else if (type === "remaining_time") {
         var other_elem = document.getElementById("next_alarm");
     } else return;
+
+    let settings_icon = document.getElementById("parameter_icon_container");
     
     if (window.toggled){
         other_elem.style = "";
+        settings_icon.style = "opacity: 100%";
     } else {
         other_elem.style = "max-height: 0px; opacity: 0%";
+        settings_icon.style = "opacity: 0%;";
     }
     window.toggled = !window.toggled;
 }
