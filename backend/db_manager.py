5fe8b04ced6216838b02e79294baea6f20809a7e
Yannis300307 <yannis300307@gmail.com>
2024-09-23 19:37:19 +0200

    Mise à jour de la doc

--- backend/db_manager.py
+++ backend/db_manager.py
@@ -118,10 +118,19 @@ help_str = \
     db_manager.py remove <name>
         Remove the provider
     
-    db_manager.py alarms <set/get> <all/day> <name> [day number]
+    db_manager.py alarms <set/get> <all/day> <name>
         Edit or get data from an alarm.
-        "all" access to all the alarms
-        "day" access to a specific day
+        set:
+            db_manager.py alarms set all <name> <value>
+            "all" access to all the alarms of the week
+            "day" access to a specific day:
+                 db_manager.py alarms set day <name> <value> <day number>
+        get:
+            db_manager.py alarms get all <name> <value>
+            "all" access to all the alarms of the week
+            "day" access to a specific day:
+                 db_manager.py alarms get day <name> <day number>
+
 """
 
 if __name__ == "__main__":
@@ -167,9 +176,9 @@ if __name__ == "__main__":
                 elif len(sys.argv) == 4:
                     print("Missing argument <name>. See `db_manager.py help` for more help.")
                 elif sys.argv[3] == "day" and len(sys.argv) == 6 and not sys.argv[5].isdigit():
-                    print("Argument [day number] must be an integer. See `db_manager.py help` for more help.")
+                    print("Argument <day number> must be an integer. See `db_manager.py help` for more help.")
                 elif sys.argv[3] == "all" and len(sys.argv) == 6:
-                    print("Argument [day number] not needed with 'all'. See `db_manager.py help` for more help.")
+                    print("Argument <day number> not needed with 'all'. See `db_manager.py help` for more help.")
                 else:
                     if sys.argv[3] == "all":
                         if len(sys.argv) > 5:
@@ -178,7 +187,7 @@ if __name__ == "__main__":
                             get_alarms(sys.argv[4], -1)
                     else:
                         if len(sys.argv) == 5:
-                            print("Missing argument [day number]. See `db_manager.py help` for more help.")
+                            print("Missing argument <day number>. See `db_manager.py help` for more help.")
                         else:
                             get_alarms(sys.argv[4], int(sys.argv[5]))
             elif sys.argv[2] == "set":
@@ -189,9 +198,9 @@ if __name__ == "__main__":
                 elif len(sys.argv) == 4:
                     print("Missing argument <name>. See `db_manager.py help` for more help.")
                 elif sys.argv[3] == "day" and len(sys.argv) == 7 and not sys.argv[6].isdigit():
-                    print("Argument [day number] must be an integer. See `db_manager.py help` for more help.")
+                    print("Argument <day number> must be an integer. See `db_manager.py help` for more help.")
                 elif sys.argv[3] == "all" and len(sys.argv) == 7:
-                    print("Argument [day number] not needed with 'all'. See `db_manager.py help` for more help.")
+                    print("Argument <day number> not needed with 'all'. See `db_manager.py help` for more help.")
                 else:
                     if sys.argv[3] == "all":
                         if len(sys.argv) > 6:
@@ -200,7 +209,7 @@ if __name__ == "__main__":
                             set_alarms(sys.argv[4], sys.argv[5])
                     else:
                         if len(sys.argv) == 5:
-                            print("Missing argument [day number]. See `db_manager.py help` for more help.")
+                            print("Missing argument <day number>. See `db_manager.py help` for more help.")
                         else:
                             set_alarms(sys.argv[4], sys.argv[5], int(sys.argv[6]))
             else:
