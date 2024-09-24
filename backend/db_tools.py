a4d78d67f42ac0fe956cd36cfd7007aa5173aa2e
Yannis300307 <yannis300307@gmail.com>
2024-09-24 13:05:18 +0200

    Renomage de db manager

--- backend/db_tools.py
+++ backend/db_tools.py
@@ -114,33 +114,33 @@ def setup_new(force=False):
 help_str = \
 """DB manager help:
 
-    db_manager.py help
+    db_tools.py help
         Show this help message
     
-    db_manager.py list
+    db_tools.py list
         List all the providers
     
-    db_manager.py setup [-f]
+    db_tools.py setup [-f]
         Setup a new database. Will ask for confirmation if the database already exist. Use `-f` to bypass asking for confirmation.
     
-    db_manager.py add <name>
+    db_tools.py add <name>
         Add a provider with no registered alarms
 
-    db_manager.py remove <name>
+    db_tools.py remove <name>
         Remove the provider
     
-    db_manager.py alarms <set/get> <all/day> <name>
+    db_tools.py alarms <set/get> <all/day> <name>
         Edit or get data from an alarm.
         set:
-            db_manager.py alarms set all <name> <value>
+            db_tools.py alarms set all <name> <value>
             "all" access to all the alarms of the week
             "day" access to a specific day:
-                 db_manager.py alarms set day <name> <value> <day number>
+                 db_tools.py alarms set day <name> <value> <day number>
         get:
-            db_manager.py alarms get all <name> <value>
+            db_tools.py alarms get all <name> <value>
             "all" access to all the alarms of the week
             "day" access to a specific day:
-                 db_manager.py alarms get day <name> <day number>
+                 db_tools.py alarms get day <name> <day number>
 
 """
 
@@ -153,12 +153,12 @@ if __name__ == "__main__":
         if len(sys.argv) == 2:
             list_providers()
         else:
-            print("Too many argument. See `db_manager.py help` for more help.")
+            print("Too many argument. See `db_tools.py help` for more help.")
     elif sys.argv[1] == "add":
         if len(sys.argv) == 2:
-            print("Missing argument <name>. See `db_manager.py help` for more help.")
+            print("Missing argument <name>. See `db_tools.py help` for more help.")
         elif len(sys.argv) > 3:
-            print("Too many argument. See `db_manager.py help` for more help.")
+            print("Too many argument. See `db_tools.py help` for more help.")
         else:
             add_provider(sys.argv[2])
     elif sys.argv[1] == "setup":
@@ -166,69 +166,69 @@ if __name__ == "__main__":
             if sys.argv[2] == "-f":
                 setup_new(True)
             else:
-                print(f"Bad argument {sys.argv[2]}. See `db_manager.py help` for more help.")
+                print(f"Bad argument {sys.argv[2]}. See `db_tools.py help` for more help.")
         elif len(sys.argv) > 3:
-            print("Too many argument. See `db_manager.py help` for more help.")
+            print("Too many argument. See `db_tools.py help` for more help.")
         else:
             setup_new()
     elif sys.argv[1] == "remove":
         if len(sys.argv) == 2:
-            print("Missing argument <name>. See `db_manager.py help` for more help.")
+            print("Missing argument <name>. See `db_tools.py help` for more help.")
         elif len(sys.argv) > 3:
-            print("Too many argument. See `db_manager.py help` for more help.")
+            print("Too many argument. See `db_tools.py help` for more help.")
         else:
             remove_provider(sys.argv[2])
     elif sys.argv[1] == "alarms":
         if len(sys.argv) == 2:
-            print("Missing argument <set/get>. See `db_manager.py help` for more help.")
+            print("Missing argument <set/get>. See `db_tools.py help` for more help.")
         elif len(sys.argv) > 7:
-            print("Too many argument. See `db_manager.py help` for more help.")
+            print("Too many argument. See `db_tools.py help` for more help.")
         else:
             if sys.argv[2] == "get":
                 if len(sys.argv) == 3:
-                    print("Missing argument <all/day>. See `db_manager.py help` for more help.")
+                    print("Missing argument <all/day>. See `db_tools.py help` for more help.")
                 elif not (sys.argv[3] == "all" or sys.argv[3] == "day"):
-                    print(f"Unknown option {sys.argv[3]}. See `db_manager.py help` for more help.")
+                    print(f"Unknown option {sys.argv[3]}. See `db_tools.py help` for more help.")
                 elif len(sys.argv) == 4:
-                    print("Missing argument <name>. See `db_manager.py help` for more help.")
+                    print("Missing argument <name>. See `db_tools.py help` for more help.")
                 elif sys.argv[3] == "day" and len(sys.argv) == 6 and not sys.argv[5].isdigit():
-                    print("Argument <day number> must be an integer. See `db_manager.py help` for more help.")
+                    print("Argument <day number> must be an integer. See `db_tools.py help` for more help.")
                 elif sys.argv[3] == "all" and len(sys.argv) == 6:
-                    print("Argument <day number> not needed with 'all'. See `db_manager.py help` for more help.")
+                    print("Argument <day number> not needed with 'all'. See `db_tools.py help` for more help.")
                 else:
                     if sys.argv[3] == "all":
                         if len(sys.argv) > 5:
-                            print("Too many argument. See `db_manager.py help` for more help.")
+                            print("Too many argument. See `db_tools.py help` for more help.")
                         else:
                             get_alarms(sys.argv[4], -1)
                     else:
                         if len(sys.argv) == 5:
-                            print("Missing argument <day number>. See `db_manager.py help` for more help.")
+                            print("Missing argument <day number>. See `db_tools.py help` for more help.")
                         else:
                             get_alarms(sys.argv[4], int(sys.argv[5]))
             elif sys.argv[2] == "set":
                 if len(sys.argv) == 3:
-                    print("Missing argument <all/day>. See `db_manager.py help` for more help.")
+                    print("Missing argument <all/day>. See `db_tools.py help` for more help.")
                 elif not (sys.argv[3] == "all" or sys.argv[3] == "day"):
-                    print(f"Unknown option {sys.argv[3]}. See `db_manager.py help` for more help.")
+                    print(f"Unknown option {sys.argv[3]}. See `db_tools.py help` for more help.")
                 elif len(sys.argv) == 4:
-                    print("Missing argument <name>. See `db_manager.py help` for more help.")
+                    print("Missing argument <name>. See `db_tools.py help` for more help.")
                 elif sys.argv[3] == "day" and len(sys.argv) == 7 and not sys.argv[6].isdigit():
-                    print("Argument <day number> must be an integer. See `db_manager.py help` for more help.")
+                    print("Argument <day number> must be an integer. See `db_tools.py help` for more help.")
                 elif sys.argv[3] == "all" and len(sys.argv) == 7:
-                    print("Argument <day number> not needed with 'all'. See `db_manager.py help` for more help.")
+                    print("Argument <day number> not needed with 'all'. See `db_tools.py help` for more help.")
                 else:
                     if sys.argv[3] == "all":
                         if len(sys.argv) > 6:
-                            print("Too many argument. See `db_manager.py help` for more help.")
+                            print("Too many argument. See `db_tools.py help` for more help.")
                         else:
                             set_alarms(sys.argv[4], sys.argv[5])
                     else:
                         if len(sys.argv) == 5:
-                            print("Missing argument <day number>. See `db_manager.py help` for more help.")
+                            print("Missing argument <day number>. See `db_tools.py help` for more help.")
                         else:
                             set_alarms(sys.argv[4], sys.argv[5], int(sys.argv[6]))
             else:
-                print(f"Unknown option {sys.argv[2]}. See `db_manager.py help` for more help.")
+                print(f"Unknown option {sys.argv[2]}. See `db_tools.py help` for more help.")
     else:
-        print(f"Unknown option {sys.argv[1]}. See `db_manager.py help` for more help.")
+        print(f"Unknown option {sys.argv[1]}. See `db_tools.py help` for more help.")
