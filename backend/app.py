a32f2b421b79dab02769c3fc248c1236326b3fe4
yannis300307 <yannis300307@gmail.com>
2024-09-29 14:00:16 +0200

    Lancement du serveur avec python et non flask

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 import json
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -22,3 +23,7 @@ def alarm(pid: str):
 @app.route("/provider_list")
 def provider_list():
     return data["providers"]
+
+if __name__ == "__main__":
+    port = 5000
+    app.run(debug=True, host='0.0.0.0', port=port)
