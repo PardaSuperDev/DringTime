1ec0bde4e04b3b137d35d6a7c08d4e090997b09f
Yannis300307 <yannis300307@gmail.com>
2024-09-29 11:51:06 +0200

    Ajout d'un message sur la page root

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,7 @@
 from flask import Flask
 
-
-
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return "<p>Dring Time Website API. Hello! </p><a href='https://dring-time.fr'>Go to the website.</a>"
