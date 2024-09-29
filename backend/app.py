5f69db45568ee0db6261bd7b75235c5fd4eb9e1b
yannis300307 <yannis300307@gmail.com>
2024-09-29 12:16:54 +0200

    Ajout de la récuperation des providers et des sonneries

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,24 @@
+import json
 from flask import Flask
 
 app = Flask(__name__)
 
+with open("data/data.json", "r") as file:
+    data = json.loads(file.read())
+    
+
 @app.route("/")
 def index():
     return "<p>Dring Time Website API. Hello! </p><a href='https://dring-time.fr'>Go to the website.</a>"
+
+
+@app.route("/alarms/<pid>")
+def alarm(pid: str):
+    if pid in data["alarms"]:
+        return data["alarms"][pid]
+    
+    return {"error": "Not found"}
+
+@app.route("/provider_list")
+def provider_list():
+    return data["providers"]
