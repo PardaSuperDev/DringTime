7f6a99d4528767ec69bf1e5f9f6641bcc15e3083
yannis300307 <yannis300307@gmail.com>
2024-09-29 18:42:15 +0200

    Update du cors pour l'API

--- frontend/index.php
+++ frontend/index.php
@@ -1,4 +1,4 @@
 <?PHP
-header('Access-Control-Allow-Origin: https://worldtimeapi.org/');
+header('Access-Control-Allow-Origin: https://worldtimeapi.org/, https://api.dring-time.fr');
 echo file_get_contents("main.html");
 ?>
